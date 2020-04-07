0e6df469cceff945827b58bd69bf5ffed29b3ec5
hcyhehe <960696163@qq.com>
2020-04-07 21:24:07 +0800

    111

---
+++ 1.js
@@ -0,0 +1,5 @@
+var c = 10;
+function add(a, b) {
+    return a + b + c;
+}
+console.log(add(3, 5));
---
+++ 1.ts
@@ -0,0 +1,7 @@
+let c: number = 10
+
+function add(a:number, b:number): number {
+    return a + b + c
+}
+
+console.log(add(3,5))
